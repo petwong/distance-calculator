e83b80260bff480db37075f38c594808904519b8
Peter Wong <peter.wong@dhl.com>
2023-06-05 17:58:18 +0800

    validator

--- src/main/java/com/petwong/distancecalculator/DistanceCalculatorController.java
+++ src/main/java/com/petwong/distancecalculator/DistanceCalculatorController.java
@@ -1,9 +1,11 @@
 package com.petwong.distancecalculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class DistanceCalculatorController {
@@ -12,6 +14,11 @@ public class DistanceCalculatorController {
 
     @GetMapping("/api/calculator/distance/sum")
     public String calculateDistance(@RequestBody DistanceCalculatorDTO dto) {
+        if (dto.getInput1().value < 0 || dto.getInput2().value < 0) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_ACCEPTABLE, "Negative Values Not Acceptable.");
+        }
+
         return distanceCalculatorService.sumOf(dto.getInput1(), dto.getInput2(), dto.getUom()).toString();
     }
 }
