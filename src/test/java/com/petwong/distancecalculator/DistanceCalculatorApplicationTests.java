c25ec6dab727c9a3644ea07282fdc14a18013874
Peter Wong <peter.wong@dhl.com>
2023-06-05 15:18:08 +0800

    Initial Commit - initialize project

---
+++ src/test/java/com/petwong/distancecalculator/DistanceCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petwong.distancecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
